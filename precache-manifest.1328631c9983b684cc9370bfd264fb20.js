2270fc440ecd8ffeba64e7f63f932506dc8434c0
Piotr Soliński <solinskip@icloud.com>
2020-05-29 11:03:59 +0200

    Updates

---
+++ precache-manifest.1328631c9983b684cc9370bfd264fb20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a58f1110728d066f0f4bf6041c7432",
+    "url": "/react-hooks-example/index.html"
+  },
+  {
+    "revision": "8edc0d6b96f5c30c8777",
+    "url": "/react-hooks-example/static/css/main.590ee4dc.chunk.css"
+  },
+  {
+    "revision": "9bd35bb1c378ed48761b",
+    "url": "/react-hooks-example/static/js/2.1310d211.chunk.js"
+  },
+  {
+    "revision": "8edc0d6b96f5c30c8777",
+    "url": "/react-hooks-example/static/js/main.6d884ee7.chunk.js"
+  },
+  {
+    "revision": "ab6ffae836e14b2c8ef2",
+    "url": "/react-hooks-example/static/js/runtime~main.a9e0d72b.js"
+  }
+]);
